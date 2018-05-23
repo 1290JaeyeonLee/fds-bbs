8b3c1b1bc1a95a4ab2b1f7a5e13bb793a845f5ad
Lee Jaeyeon <1290jaeyeonlee@gmail.com>
2018-05-23 11:54:16 +0900

    json server 생성

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import './index.scss';
+import axios from 'axios';
 
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
-});
+axios.get('http://localhost:3000/posts').then(res => {
+  res.data.forEach(post => {
+    //임시로 보관하는 통
+    const fragment = document.importNode(document.querySelector('#post-item').content, true);
+    // const pEl = document.createElement('p');
+    const pEl = fragment.querySelector('.post-item__title');
+    pEl.textContent = post.title;
+    // document.querySelector('.post-list').appendChild(pEl);
+    document.querySelector('.post-list').appendChild(fragment);
+  })
+})
